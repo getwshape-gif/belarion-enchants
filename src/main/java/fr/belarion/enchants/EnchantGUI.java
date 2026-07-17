f0083a3999fce0a5cba4663c75f07204a110beda
getwshape-gif <getwshape@gmail.com>
2026-07-17 09:53:35 -0700

    Update EnchantGUI.java

--- src/main/java/fr/belarion/enchants/EnchantGUI.java
+++ src/main/java/fr/belarion/enchants/EnchantGUI.java
@@ -11,34 +11,88 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public final class EnchantGUI {
 
     public static final String TITLE = ChatColor.DARK_GREEN.toString() + ChatColor.BOLD + "Table d'Enchantement";
+    public static final String TITLE_LIST = ChatColor.DARK_GREEN.toString() + ChatColor.BOLD + "Enchants Disponibles";
     public static final int COST_LEVELS = 60;
-    public static final String SLOT_KEY = "belslot";
 
-    private static final int[] DISPLAY_SLOTS = new int[]{11, 13, 15};
+    public static final int SLOT_BOOK = 11;
+    public static final int SLOT_ENCHANT = 15;
+    public static final int SLOT_LIST = 22;
+    public static final int SLOT_BACK = 22;
+
+    private static final Random RANDOM = new Random();
 
     private EnchantGUI() {}
 
     public static Inventory build() {
         Inventory inv = Bukkit.createInventory(null, 27, TITLE);
 
-        CustomEnchant[] enchants = CustomEnchant.values();
-        for (int i = 0; i < enchants.length && i < DISPLAY_SLOTS.length; i++) {
-            inv.setItem(DISPLAY_SLOTS[i], buildDisplayItem(enchants[i]));
+        ItemStack green = pane((short) 13);
+        ItemStack lime = pane((short) 5);
+        for (int i = 0; i < 27; i++) {
+            inv.setItem(i, (i % 2 == 0) ? green : lime);
         }
 
-        ItemStack info = new ItemStack(Material.EXP_BOTTLE);
+        ItemStack info = new ItemStack(Material.ENCHANTMENT_TABLE);
         ItemMeta infoMeta = info.getItemMeta();
-        infoMeta.setDisplayName(ChatColor.GREEN + "Coût : " + COST_LEVELS + " niveaux");
+        infoMeta.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "Table d'Enchantement en \u00c9meraude");
         List<String> infoLore = new ArrayList<String>();
-        infoLore.add(ChatColor.GRAY + "Clique sur un enchant pour");
-        infoLore.add(ChatColor.GRAY + "recevoir son livre custom.");
+        infoLore.add(ChatColor.GRAY + "Place un livre vide dans la case,");
+        infoLore.add(ChatColor.GRAY + "clique sur Enchanter et re\u00e7ois un");
+        infoLore.add(ChatColor.GRAY + "enchant custom al\u00e9atoire !");
         infoMeta.setLore(infoLore);
         info.setItemMeta(infoMeta);
         inv.setItem(4, info);
+
+        inv.setItem(SLOT_BOOK, null);
+
+        ItemStack enchant = new ItemStack(Material.EXP_BOTTLE);
+        ItemMeta em = enchant.getItemMeta();
+        em.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "Enchanter");
+        List<String> el = new ArrayList<String>();
+        el.add(ChatColor.GRAY + "Co\u00fbt : " + ChatColor.YELLOW + COST_LEVELS + " niveaux");
+        el.add(ChatColor.GRAY + "R\u00e9sultat : " + ChatColor.LIGHT_PURPLE + "un enchant al\u00e9atoire");
+        em.setLore(el);
+        enchant.setItemMeta(em);
+        inv.setItem(SLOT_ENCHANT, enchant);
+
+        ItemStack list = new ItemStack(Material.BOOK_AND_QUILL);
+        ItemMeta lm = list.getItemMeta();
+        lm.setDisplayName(ChatColor.YELLOW.toString() + ChatColor.BOLD + "Enchants disponibles");
+        List<String> ll = new ArrayList<String>();
+        ll.add(ChatColor.GRAY + "Clique pour voir la liste.");
+        lm.setLore(ll);
+        list.setItemMeta(lm);
+        inv.setItem(SLOT_LIST, list);
+
+        return inv;
+    }
+
+    public static Inventory buildList() {
+        Inventory inv = Bukkit.createInventory(null, 27, TITLE_LIST);
+
+        ItemStack green = pane((short) 13);
+        ItemStack lime = pane((short) 5);
+        for (int i = 0; i < 27; i++) {
+            inv.setItem(i, (i % 2 == 0) ? green : lime);
+        }
+
+        int slot = 10;
+        for (CustomEnchant enchant : CustomEnchant.values()) {
+            if (slot > 16) break;
+            inv.setItem(slot, buildDisplayItem(enchant));
+            slot++;
+        }
+
+        ItemStack back = new ItemStack(Material.ARROW);
+        ItemMeta bm = back.getItemMeta();
+        bm.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Retour");
+        back.setItemMeta(bm);
+        inv.setItem(SLOT_BACK, back);
 
         return inv;
     }
@@ -47,45 +101,57 @@ public final class EnchantGUI {
         ItemStack item = new ItemStack(Material.ENCHANTED_BOOK);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(enchant.getColor().toString() + ChatColor.BOLD + enchant.getDisplayName());
-
         List<String> lore = new ArrayList<String>();
         for (String line : enchant.getLore()) {
             lore.add(ChatColor.GRAY + line);
         }
-        lore.add("");
-        lore.add(ChatColor.YELLOW + "Coût : " + COST_LEVELS + " niveaux");
-        lore.add(ChatColor.GREEN + "Clique pour enchanter !");
         meta.setLore(lore);
         item.setItemMeta(meta);
-
-        HiddenTag.write(item, SLOT_KEY, enchant.getId());
         return item;
     }
 
-    public static CustomEnchant getEnchantFromClickedItem(ItemStack item) {
-        String id = HiddenTag.read(item, SLOT_KEY);
-        if (id == null) return null;
-        return CustomEnchant.fromId(id);
+    private static ItemStack pane(short color) {
+        ItemStack filler = new ItemStack(Material.STAINED_GLASS_PANE, 1, color);
+        ItemMeta fm = filler.getItemMeta();
+        fm.setDisplayName(" ");
+        filler.setItemMeta(fm);
+        return filler;
     }
 
-    public static boolean tryPurchase(Player player, CustomEnchant enchant) {
+    public static void tryEnchant(Player player, Inventory top) {
+        ItemStack book = top.getItem(SLOT_BOOK);
+        if (book == null || book.getType() != Material.BOOK) {
+            player.sendMessage(ChatColor.RED + "Place un livre vide dans la case \u00e0 gauche.");
+            player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1f, 1f);
+            return;
+        }
+
         if (player.getLevel() < COST_LEVELS) {
             player.sendMessage(ChatColor.RED + "Il te manque des niveaux ! (" + COST_LEVELS
                     + " requis, tu en as " + player.getLevel() + ")");
             player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1f, 1f);
-            return false;
+            return;
         }
 
+        if (book.getAmount() > 1) {
+            book.setAmount(book.getAmount() - 1);
+            top.setItem(SLOT_BOOK, book);
+        } else {
+            top.setItem(SLOT_BOOK, null);
+        }
         player.setLevel(player.getLevel() - COST_LEVELS);
 
-        ItemStack book = EnchantBookUtil.createBook(enchant);
-        java.util.Map<Integer, ItemStack> leftovers = player.getInventory().addItem(book);
+        CustomEnchant[] all = CustomEnchant.values();
+        CustomEnchant picked = all[RANDOM.nextInt(all.length)];
+
+        ItemStack result = EnchantBookUtil.createBook(picked);
+        java.util.Map<Integer, ItemStack> leftovers = player.getInventory().addItem(result);
         for (ItemStack leftover : leftovers.values()) {
             player.getWorld().dropItem(player.getLocation(), leftover);
         }
 
-        player.sendMessage(ChatColor.GREEN + "Tu as reçu : " + enchant.getColor() + "Livre : " + enchant.getDisplayName());
-        player.playSound(player.getLocation(), Sound.LEVEL_UP, 1f, 1.4f);
-        return true;
+        player.sendMessage(ChatColor.GREEN + "Le livre s'illumine... Tu obtiens : "
+                + picked.getColor() + ChatColor.BOLD + picked.getDisplayName());
+        player.playSound(player.getLocation(), Sound.LEVEL_UP, 1f, 1.2f);
     }
 }
